01c4cd67e6f4f081c4f58075619968aebc228cf6
jackie <jdimery2022@gmail.com>
2022-08-03 21:21:10 -0700

    corrected js final

---
+++ README.md
@@ -0,0 +1,8 @@
+# JavaScript Final: magic8ball. 
+Created an input field for users to ask a question
+Created a label for the input field telling your user to "Ask me anything! ...."
+
+
+Replaced the input field with the user's question, the response will be displayed where the 8 was.  
+Styled page with rainy background
+Activated Github Page for this project
---
+++ js/main.js
@@ -0,0 +1,38 @@
+const responses = ["Hazy with a 99% chance of No", 
+    "Think about that question and ask again",
+    "Now is not a good time, ask again in about 90 days",
+    "For sure",
+    "Doubtful",
+    "Ask again later in about one year",
+    "Don't rely on it",
+    "Yes, maybe",
+    "It's possible",
+    "No",
+    "Dream on",
+    "Outlook is sunny with a 99% chance of Yes",
+    "Better not tell you now, ask in 30 days",
+    "Signs point to yes",
+    "Don't count on it",
+    "No",
+    "Outlook stormy with a 75% chance of No",
+    "Yes - in 100 light years",
+];
+
+	
+
+window.onload = function () {
+    const numberEight = document.getElementById("numberEight");
+    const response = document.getElementById("response");
+    const eightBall = document.getElementById("eight-ball");
+    const question = document.getElementById("question");
+    eightBall.addEventListener("click", function () {
+        if (question.value.length < 1) {
+            alert('What do you want to know?');
+        } else {
+            
+            numberEight.innerText = "";
+            var num = Math.floor(Math.random() * Math.floor(responses.length));
+            response.innerText = responses[num];
+        }
+    });
+};
